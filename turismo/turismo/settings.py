26a513d752e6ac8475c053f444704df5b8b61dd4
giocastagno <giorginacastagno@hotmail.com>
2017-04-22 18:53:31 -0300

    Add files via upload

--- turismo/turismo/settings.py
+++ turismo/turismo/settings.py
@@ -132,3 +132,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_server_files')
+
+if os.environ.get('HEROKU', False):
+    # settings especificas para heroku
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config()
+    ALLOWED_HOSTS = ['*']
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
