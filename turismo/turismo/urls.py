70a4806ccf0fc4b1de18199f5ff03961df7cf827
Giorgi <giorginacastagno@hotmail.com>
2017-04-26 17:22:33 -0300

    corregido subir imagenes

--- turismo/turismo/urls.py
+++ turismo/turismo/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
+
 
 from sitio.views import inicio, crear_itinerario, usuario
 urlpatterns = [
@@ -24,4 +26,4 @@ urlpatterns = [
     url(r'^crear_itinerario/$', crear_itinerario),
     url(r'^usuario/$', usuario),
     url(r'^accounts/', include('registration.backends.hmac.urls')),
-    ]
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
