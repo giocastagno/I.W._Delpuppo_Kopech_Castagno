f8376ec4948ddce0ca902829eb3f1a683c9ec052
Giorgi <giorginacastagno@hotmail.com>
2017-04-26 17:51:14 -0300

    agregado modelo Comentario

--- turismo/sitio/forms.py
+++ turismo/sitio/forms.py
@@ -1,12 +1,17 @@
 from django import forms
 
-from sitio.models import Itinerario
+from sitio.models import Itinerario, Dia
 
 
 class ItinerarioForm(forms.ModelForm):
     class Meta:
         model = Itinerario
         exclude = ['fecha', 'usuario', 'estado', ]
+
+class DiaForm(forms.ModelForm):
+	class Meta:
+		model = Dia 
+		exclude = ['fecha','usuario','itinerario',]
 
 
 
