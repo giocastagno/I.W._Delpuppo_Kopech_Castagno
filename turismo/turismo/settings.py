704d72d4591811f80a2eba26189a09d8b17fcc9f
Giorgi <giorginacastagno@hotmail.com>
2017-04-29 16:05:49 -0300

    arreglado settings.py

--- turismo/turismo/settings.py
+++ turismo/turismo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'aes17g059t1gznj=u2x#$x(e*708@hw&&^6+(^yzb28_yrv851'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
