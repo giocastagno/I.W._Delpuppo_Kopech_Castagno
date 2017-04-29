7f07b4e8287dfd3320c0ced362a3da96863ad528
Giorgi <giorginacastagno@hotmail.com>
2017-04-29 16:12:51 -0300

    cambiadas las settings.py

--- turismo/turismo/settings.py
+++ turismo/turismo/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'aes17g059t1gznj=u2x#$x(e*708@hw&&^6+(^yzb28_yrv851'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
@@ -133,11 +132,16 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_server_files')
 
 if os.environ.get('HEROKU', False):
     # settings especificas para heroku
+    DEBUG = False
     import dj_database_url
     DATABASES['default'] = dj_database_url.config()
     ALLOWED_HOSTS = ['*']
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+else:
+    DEBUG = True
+
