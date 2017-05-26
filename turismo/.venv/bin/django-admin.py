abfc41221e41c4a0be79f1acad2bba2556a81bfc
Giorgi <giorginacastagno@hotmail.com>
2017-05-25 22:55:42 -0300

    arregladas algunas cosas para la entrega3 y 4

---
+++ turismo/.venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mdelpuppo/seia_dkc/I.W._Delpuppo_Kopech_Castagno/turismo/.venv/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
