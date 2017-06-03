3df3256ce35fc23e3317f0d6a49b19f8489ef49e
Giorgi <giorginacastagno@hotmail.com>
2017-06-03 15:24:31 -0300

    ultimas correciones

--- turismo/sitio/forms.py
+++ turismo/sitio/forms.py
@@ -9,18 +9,34 @@ class ItinerarioForm(forms.ModelForm):
         model = Itinerario
         widgets = {
         	'titulo': forms.Textarea(attrs={'cols': 50, 'rows': 1, 'style':'resize:none;'}),
-            'texto_general': SummernoteInplaceWidget(),
+            'texto_general': SummernoteWidget(attrs={
+            	'width': '100%',
+        		'height': '300',
+				'toolbar': [
+			    	['style', ['bold', 'italic', 'underline', 'clear']],
+			    	['font', ['strikethrough', 'superscript', 'subscript']],
+			    	['fontsize', ['fontsize']],
+			    	['color', ['color']],
+			    	['para', ['ul', 'ol', 'paragraph']],
+			    	['height', ['height']]]
+				}),
             'fecha_salida': DateWidget(attrs={'usel10n':'True', 'bootstrap_version':'3'}),
             'fecha_llegada': DateWidget(attrs={'usel10n':'True', 'bootstrap_version':'3'}),
-            	}
+            }
         exclude = ['fecha', 'usuario', 'estado', 'valoracion', 'visitas',]
 
 class DiaForm(forms.ModelForm):
 	class Meta:
 		model = Dia
 		widgets = {
-			'descripcion': SummernoteInplaceWidget(),
-			} 
+			'descripcion': SummernoteWidget({
+        	'height': '150',
+			'toolbar': [
+			    ['style', ['bold', 'italic', 'underline', 'clear']],
+			    ['fontsize', ['fontsize']],
+			    ['color', ['color']]]
+			}),
+		} 
 		exclude = ['fecha','usuario','itinerario',]
 
 class PerfilForm(forms.ModelForm):
@@ -32,8 +48,12 @@ class ComentarioForm(forms.ModelForm):
 	class Meta:
 		model = Comentario
 		widgets = {
-        	'texto': SummernoteInplaceWidget(),
-            	}
+        	'texto': SummernoteWidget({
+        	'height': '150',
+			'toolbar': [
+			    ['style', ['bold', 'italic', 'underline', 'clear']]]
+			}),
+        }
 		exclude = ['usuario', 'fecha', 'itinerario', 'denuncias',]
 
 class PuntajeForm(forms.ModelForm):
